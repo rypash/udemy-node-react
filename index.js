2e1a6077d96209f4e714cd85898e87a2cdcb2901
Ryland <rylandpashovitz@hotmail.ca>
2018-10-12 15:05:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hey: 'you' });
+  res.send({ hey: 'boy' });
 });
 
 const PORT = process.env.PORT || 5500;
